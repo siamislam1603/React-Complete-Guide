5e7b8ad00078466048e474887a2ac05163007896
siamislam1603 <30402883+siamislam1603@users.noreply.github.com>
2020-07-24 21:33:24 +0600

    Manipulating state changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import person from './Person/Person';
 class App extends Component {
+  state={
+    persons:[
+      {name:'Max',age:23},
+      {name:'Sam',age:29},
+      {name:'Manu',age:32}
+    ]
+  }
+  switchNameHandler=()=>{
+    this.setState({
+      persons:[
+        {name:'Maximilian',age:23},
+        {name:'Samuel',age:29},
+        {name:'Manuer',age:32}
+      ]
+    });
+  }
   render() {
     return (
       <div className="App">
         <h1>Hello There!</h1>
-        <Person name="Max" age="23" />
-        <Person name="Sam" age="29" />
-        <Person name="Xung" age="32">My Hobbies:Gandening</Person>
+        <button onClick={this.switchNameHandler}>Switch names</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies:Gandening</Person>
       </div>
     );
   }
