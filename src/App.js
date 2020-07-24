d3ad4f769dd1af62bab097e443028fc8a4d3e78e
siamislam1603 <30402883+siamislam1603@users.noreply.github.com>
2020-07-24 22:09:35 +0600

    Using the useState() Hook for State Manipulation

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
-class App extends Component {
-  state={
+const App =(props)=> {
+
+  // Array destructuring
+  const [personsState,setPersonsState]=useState({
     persons:[
       {name:'Max',age:23},
       {name:'Sam',age:29},
       {name:'Manu',age:32}
     ]
-  }
-  switchNameHandler=()=>{
-    this.setState({
+  });
+  
+  //React hooks doesn't merge old states with changing ones but class based component does
+  const [otherState,setOtherState]=useState('some other value');
+  console.log(personsState,otherState);
+
+  const switchNameHandler=()=>{
+    setPersonsState({
       persons:[
         {name:'Maximilian',age:23},
         {name:'Samuel',age:29},
         {name:'Manuer',age:32}
       ]
     });
-  }
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello There!</h1>
-        <button onClick={this.switchNameHandler}>Switch names</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies:Gandening</Person>
-      </div>
-    );
-  }
+  };
+  return (
+    <div className="App">
+      <h1>Hello There!</h1>
+      <button onClick={switchNameHandler}>Switch names</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>My Hobbies:Gandening</Person>
+    </div>
+  );
+  
 }
 
 export default App;
